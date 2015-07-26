4d11cbb1a4422a677d73549b428b4622f197e8d5
Martin Schulz <MartinSchulz2000@gmail.com>
2015-07-26 17:02:28 +0200

    Standard Funktionen fertig

--- Placeholder Image/Form1.Designer.cs
+++ Placeholder Image/Form1.Designer.cs
@@ -28,20 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gb_service = new System.Windows.Forms.GroupBox();
+            this.rb_placeholdit = new System.Windows.Forms.RadioButton();
+            this.rb_placekitten = new System.Windows.Forms.RadioButton();
+            this.l_width = new System.Windows.Forms.Label();
+            this.l_height = new System.Windows.Forms.Label();
+            this.l_text = new System.Windows.Forms.Label();
+            this.tb_text = new System.Windows.Forms.TextBox();
+            this.pb_image = new System.Windows.Forms.PictureBox();
+            this.bt_go = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tb_width = new System.Windows.Forms.NumericUpDown();
+            this.tb_height = new System.Windows.Forms.NumericUpDown();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.bt_save = new System.Windows.Forms.Button();
+            this.gb_service.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_height)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gb_service
+            // 
+            this.gb_service.Controls.Add(this.rb_placekitten);
+            this.gb_service.Controls.Add(this.rb_placeholdit);
+            this.gb_service.Location = new System.Drawing.Point(12, 12);
+            this.gb_service.Name = "gb_service";
+            this.gb_service.Size = new System.Drawing.Size(200, 100);
+            this.gb_service.TabIndex = 0;
+            this.gb_service.TabStop = false;
+            this.gb_service.Text = "Service";
+            // 
+            // rb_placeholdit
+            // 
+            this.rb_placeholdit.AutoSize = true;
+            this.rb_placeholdit.Location = new System.Drawing.Point(6, 19);
+            this.rb_placeholdit.Name = "rb_placeholdit";
+            this.rb_placeholdit.Size = new System.Drawing.Size(79, 17);
+            this.rb_placeholdit.TabIndex = 0;
+            this.rb_placeholdit.TabStop = true;
+            this.rb_placeholdit.Text = "placehold.it";
+            this.rb_placeholdit.UseVisualStyleBackColor = true;
+            this.rb_placeholdit.CheckedChanged += new System.EventHandler(this.rb_placeholdit_CheckedChanged);
+            // 
+            // rb_placekitten
+            // 
+            this.rb_placekitten.AutoSize = true;
+            this.rb_placekitten.Location = new System.Drawing.Point(6, 42);
+            this.rb_placekitten.Name = "rb_placekitten";
+            this.rb_placekitten.Size = new System.Drawing.Size(77, 17);
+            this.rb_placekitten.TabIndex = 1;
+            this.rb_placekitten.TabStop = true;
+            this.rb_placekitten.Text = "placekitten";
+            this.rb_placekitten.UseVisualStyleBackColor = true;
+            this.rb_placekitten.CheckedChanged += new System.EventHandler(this.rb_placekitten_CheckedChanged);
+            // 
+            // l_width
+            // 
+            this.l_width.AutoSize = true;
+            this.l_width.Location = new System.Drawing.Point(12, 120);
+            this.l_width.Name = "l_width";
+            this.l_width.Size = new System.Drawing.Size(38, 13);
+            this.l_width.TabIndex = 1;
+            this.l_width.Text = "Width:";
+            // 
+            // l_height
+            // 
+            this.l_height.AutoSize = true;
+            this.l_height.Location = new System.Drawing.Point(12, 146);
+            this.l_height.Name = "l_height";
+            this.l_height.Size = new System.Drawing.Size(41, 13);
+            this.l_height.TabIndex = 1;
+            this.l_height.Text = "Height:";
+            // 
+            // l_text
+            // 
+            this.l_text.AutoSize = true;
+            this.l_text.Location = new System.Drawing.Point(12, 173);
+            this.l_text.Name = "l_text";
+            this.l_text.Size = new System.Drawing.Size(31, 13);
+            this.l_text.TabIndex = 1;
+            this.l_text.Text = "Text:";
+            // 
+            // tb_text
+            // 
+            this.tb_text.Location = new System.Drawing.Point(59, 170);
+            this.tb_text.Name = "tb_text";
+            this.tb_text.Size = new System.Drawing.Size(153, 20);
+            this.tb_text.TabIndex = 2;
+            // 
+            // pb_image
+            // 
+            this.pb_image.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_image.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_image.Location = new System.Drawing.Point(218, 12);
+            this.pb_image.Name = "pb_image";
+            this.pb_image.Size = new System.Drawing.Size(254, 237);
+            this.pb_image.TabIndex = 3;
+            this.pb_image.TabStop = false;
+            // 
+            // bt_go
+            // 
+            this.bt_go.Location = new System.Drawing.Point(12, 226);
+            this.bt_go.Name = "bt_go";
+            this.bt_go.Size = new System.Drawing.Size(90, 23);
+            this.bt_go.TabIndex = 4;
+            this.bt_go.Text = "Go!";
+            this.bt_go.UseVisualStyleBackColor = true;
+            this.bt_go.Click += new System.EventHandler(this.bt_go_Click);
+            // 
+            // tb_width
+            // 
+            this.tb_width.Location = new System.Drawing.Point(59, 118);
+            this.tb_width.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.tb_width.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tb_width.Name = "tb_width";
+            this.tb_width.Size = new System.Drawing.Size(153, 20);
+            this.tb_width.TabIndex = 5;
+            this.tb_width.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // tb_height
+            // 
+            this.tb_height.Location = new System.Drawing.Point(59, 144);
+            this.tb_height.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.tb_height.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tb_height.Name = "tb_height";
+            this.tb_height.Size = new System.Drawing.Size(153, 20);
+            this.tb_height.TabIndex = 5;
+            this.tb_height.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Filter = "Portable Network Graphics|*.png";
+            this.saveFileDialog1.Title = "Save an Image File";
+            // 
+            // bt_save
+            // 
+            this.bt_save.Enabled = false;
+            this.bt_save.Location = new System.Drawing.Point(122, 226);
+            this.bt_save.Name = "bt_save";
+            this.bt_save.Size = new System.Drawing.Size(90, 23);
+            this.bt_save.TabIndex = 6;
+            this.bt_save.Text = "Save";
+            this.bt_save.UseVisualStyleBackColor = true;
+            this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.bt_save);
+            this.Controls.Add(this.tb_height);
+            this.Controls.Add(this.tb_width);
+            this.Controls.Add(this.bt_go);
+            this.Controls.Add(this.pb_image);
+            this.Controls.Add(this.tb_text);
+            this.Controls.Add(this.l_text);
+            this.Controls.Add(this.l_height);
+            this.Controls.Add(this.l_width);
+            this.Controls.Add(this.gb_service);
             this.Name = "Form1";
             this.Text = "Placeholder Image";
+            this.gb_service.ResumeLayout(false);
+            this.gb_service.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_height)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gb_service;
+        private System.Windows.Forms.RadioButton rb_placekitten;
+        private System.Windows.Forms.RadioButton rb_placeholdit;
+        private System.Windows.Forms.Label l_width;
+        private System.Windows.Forms.Label l_height;
+        private System.Windows.Forms.Label l_text;
+        private System.Windows.Forms.TextBox tb_text;
+        private System.Windows.Forms.PictureBox pb_image;
+        private System.Windows.Forms.Button bt_go;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown tb_width;
+        private System.Windows.Forms.NumericUpDown tb_height;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button bt_save;
     }
 }
 
--- Placeholder Image/Form1.cs
+++ Placeholder Image/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Placeholder_Image
 {
@@ -15,6 +17,47 @@ namespace Placeholder_Image
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void rb_placeholdit_CheckedChanged(object sender, EventArgs e)
+        {
+            l_text.Enabled = true;
+            tb_text.Enabled = true;
+        }
+
+        private void rb_placekitten_CheckedChanged(object sender, EventArgs e)
+        {
+            l_text.Enabled = false;
+            tb_text.Enabled = false;
+        }
+
+        private void bt_go_Click(object sender, EventArgs e)
+        {
+            if (rb_placeholdit.Checked == true)
+            {
+                pb_image.Load("http://placehold.it/" + tb_width.Value + "x" + tb_height.Value + "?text=" + tb_text.Text);
+                bt_save.Enabled = true;
+            }
+            else if (rb_placekitten.Checked == true)
+            {
+                try
+                {
+                    pb_image.Load("http://placekitten.com/g/" + tb_width.Value + "/" + tb_height.Value);
+                    bt_save.Enabled = true;
+                }
+                catch
+                {
+                    MessageBox.Show("404\nImage not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bt_save.Enabled = false;
+                }
+            }
+        }
+
+        private void bt_save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            pb_image.Image.Save(saveFileDialog1.FileName, ImageFormat.Png);
+
         }
     }
 }
