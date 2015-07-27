c1e53018cd749633ce0946b9c1b2ed46d78469e3
Martin Schulz <MartinSchulz2000@gmail.com>
2015-07-27 14:10:40 +0200

    Layout geändert Enter Taste -> Go!

--- Placeholder Image/Form1.Designer.cs
+++ Placeholder Image/Form1.Designer.cs
@@ -28,18 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gb_service = new System.Windows.Forms.GroupBox();
-            this.rb_placekitten = new System.Windows.Forms.RadioButton();
-            this.rb_placeholdit = new System.Windows.Forms.RadioButton();
+            this.gb_size = new System.Windows.Forms.GroupBox();
+            this.tb_height = new System.Windows.Forms.NumericUpDown();
             this.l_width = new System.Windows.Forms.Label();
             this.l_height = new System.Windows.Forms.Label();
+            this.tb_width = new System.Windows.Forms.NumericUpDown();
             this.l_text = new System.Windows.Forms.Label();
             this.tb_text = new System.Windows.Forms.TextBox();
             this.pb_image = new System.Windows.Forms.PictureBox();
             this.bt_go = new System.Windows.Forms.Button();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.tb_width = new System.Windows.Forms.NumericUpDown();
-            this.tb_height = new System.Windows.Forms.NumericUpDown();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.bt_save = new System.Windows.Forms.Button();
             this.l_bgcolour = new System.Windows.Forms.Label();
@@ -49,51 +47,57 @@
             this.tb_tcolour = new System.Windows.Forms.TextBox();
             this.bt_tcolour = new System.Windows.Forms.Button();
             this.cb_gray = new System.Windows.Forms.CheckBox();
-            this.gb_service.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).BeginInit();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tp_placeholdit = new System.Windows.Forms.TabPage();
+            this.tp_placekitten = new System.Windows.Forms.TabPage();
+            this.gb_size.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tb_height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tp_placeholdit.SuspendLayout();
+            this.tp_placekitten.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gb_service
+            // gb_size
             // 
-            this.gb_service.Controls.Add(this.rb_placekitten);
-            this.gb_service.Controls.Add(this.rb_placeholdit);
-            this.gb_service.Location = new System.Drawing.Point(12, 12);
-            this.gb_service.Name = "gb_service";
-            this.gb_service.Size = new System.Drawing.Size(200, 100);
-            this.gb_service.TabIndex = 0;
-            this.gb_service.TabStop = false;
-            this.gb_service.Text = "Service";
+            this.gb_size.Controls.Add(this.tb_height);
+            this.gb_size.Controls.Add(this.l_width);
+            this.gb_size.Controls.Add(this.l_height);
+            this.gb_size.Controls.Add(this.tb_width);
+            this.gb_size.Location = new System.Drawing.Point(12, 12);
+            this.gb_size.Name = "gb_size";
+            this.gb_size.Size = new System.Drawing.Size(200, 74);
+            this.gb_size.TabIndex = 0;
+            this.gb_size.TabStop = false;
+            this.gb_size.Text = "Size";
             // 
-            // rb_placekitten
+            // tb_height
             // 
-            this.rb_placekitten.AutoSize = true;
-            this.rb_placekitten.Location = new System.Drawing.Point(6, 42);
-            this.rb_placekitten.Name = "rb_placekitten";
-            this.rb_placekitten.Size = new System.Drawing.Size(77, 17);
-            this.rb_placekitten.TabIndex = 1;
-            this.rb_placekitten.Text = "placekitten";
-            this.rb_placekitten.UseVisualStyleBackColor = true;
-            this.rb_placekitten.CheckedChanged += new System.EventHandler(this.rb_placekitten_CheckedChanged);
-            // 
-            // rb_placeholdit
-            // 
-            this.rb_placeholdit.AutoSize = true;
-            this.rb_placeholdit.Checked = true;
-            this.rb_placeholdit.Location = new System.Drawing.Point(6, 19);
-            this.rb_placeholdit.Name = "rb_placeholdit";
-            this.rb_placeholdit.Size = new System.Drawing.Size(79, 17);
-            this.rb_placeholdit.TabIndex = 0;
-            this.rb_placeholdit.TabStop = true;
-            this.rb_placeholdit.Text = "placehold.it";
-            this.rb_placeholdit.UseVisualStyleBackColor = true;
-            this.rb_placeholdit.CheckedChanged += new System.EventHandler(this.rb_placeholdit_CheckedChanged);
+            this.tb_height.Location = new System.Drawing.Point(50, 45);
+            this.tb_height.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.tb_height.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tb_height.Name = "tb_height";
+            this.tb_height.Size = new System.Drawing.Size(144, 20);
+            this.tb_height.TabIndex = 5;
+            this.tb_height.Value = new decimal(new int[] {
+            285,
+            0,
+            0,
+            0});
             // 
             // l_width
             // 
             this.l_width.AutoSize = true;
-            this.l_width.Location = new System.Drawing.Point(12, 120);
+            this.l_width.Location = new System.Drawing.Point(6, 21);
             this.l_width.Name = "l_width";
             this.l_width.Size = new System.Drawing.Size(38, 13);
             this.l_width.TabIndex = 1;
@@ -102,16 +106,38 @@
             // l_height
             // 
             this.l_height.AutoSize = true;
-            this.l_height.Location = new System.Drawing.Point(12, 146);
+            this.l_height.Location = new System.Drawing.Point(6, 47);
             this.l_height.Name = "l_height";
             this.l_height.Size = new System.Drawing.Size(41, 13);
             this.l_height.TabIndex = 1;
             this.l_height.Text = "Height:";
             // 
+            // tb_width
+            // 
+            this.tb_width.Location = new System.Drawing.Point(50, 19);
+            this.tb_width.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.tb_width.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tb_width.Name = "tb_width";
+            this.tb_width.Size = new System.Drawing.Size(144, 20);
+            this.tb_width.TabIndex = 5;
+            this.tb_width.Value = new decimal(new int[] {
+            252,
+            0,
+            0,
+            0});
+            // 
             // l_text
             // 
             this.l_text.AutoSize = true;
-            this.l_text.Location = new System.Drawing.Point(12, 173);
+            this.l_text.Location = new System.Drawing.Point(6, 9);
             this.l_text.Name = "l_text";
             this.l_text.Size = new System.Drawing.Size(31, 13);
             this.l_text.TabIndex = 1;
@@ -119,9 +145,9 @@
             // 
             // tb_text
             // 
-            this.tb_text.Location = new System.Drawing.Point(59, 170);
+            this.tb_text.Location = new System.Drawing.Point(47, 6);
             this.tb_text.Name = "tb_text";
-            this.tb_text.Size = new System.Drawing.Size(153, 20);
+            this.tb_text.Size = new System.Drawing.Size(139, 20);
             this.tb_text.TabIndex = 2;
             // 
             // pb_image
@@ -147,50 +173,6 @@
             this.bt_go.UseVisualStyleBackColor = true;
             this.bt_go.Click += new System.EventHandler(this.bt_go_Click);
             // 
-            // tb_width
-            // 
-            this.tb_width.Location = new System.Drawing.Point(59, 118);
-            this.tb_width.Maximum = new decimal(new int[] {
-            1000000,
-            0,
-            0,
-            0});
-            this.tb_width.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.tb_width.Name = "tb_width";
-            this.tb_width.Size = new System.Drawing.Size(153, 20);
-            this.tb_width.TabIndex = 5;
-            this.tb_width.Value = new decimal(new int[] {
-            252,
-            0,
-            0,
-            0});
-            // 
-            // tb_height
-            // 
-            this.tb_height.Location = new System.Drawing.Point(59, 144);
-            this.tb_height.Maximum = new decimal(new int[] {
-            1000000,
-            0,
-            0,
-            0});
-            this.tb_height.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.tb_height.Name = "tb_height";
-            this.tb_height.Size = new System.Drawing.Size(153, 20);
-            this.tb_height.TabIndex = 5;
-            this.tb_height.Value = new decimal(new int[] {
-            285,
-            0,
-            0,
-            0});
-            // 
             // saveFileDialog1
             // 
             this.saveFileDialog1.DefaultExt = "png";
@@ -211,7 +193,7 @@
             // l_bgcolour
             // 
             this.l_bgcolour.AutoSize = true;
-            this.l_bgcolour.Location = new System.Drawing.Point(12, 199);
+            this.l_bgcolour.Location = new System.Drawing.Point(6, 35);
             this.l_bgcolour.Name = "l_bgcolour";
             this.l_bgcolour.Size = new System.Drawing.Size(58, 13);
             this.l_bgcolour.TabIndex = 1;
@@ -219,14 +201,14 @@
             // 
             // tb_bgcolour
             // 
-            this.tb_bgcolour.Location = new System.Drawing.Point(76, 196);
+            this.tb_bgcolour.Location = new System.Drawing.Point(70, 32);
             this.tb_bgcolour.Name = "tb_bgcolour";
-            this.tb_bgcolour.Size = new System.Drawing.Size(110, 20);
+            this.tb_bgcolour.Size = new System.Drawing.Size(93, 20);
             this.tb_bgcolour.TabIndex = 2;
             // 
             // bt_bgcolour
             // 
-            this.bt_bgcolour.Location = new System.Drawing.Point(192, 195);
+            this.bt_bgcolour.Location = new System.Drawing.Point(169, 32);
             this.bt_bgcolour.Name = "bt_bgcolour";
             this.bt_bgcolour.Size = new System.Drawing.Size(20, 20);
             this.bt_bgcolour.TabIndex = 7;
@@ -237,7 +219,7 @@
             // l_tcolour
             // 
             this.l_tcolour.AutoSize = true;
-            this.l_tcolour.Location = new System.Drawing.Point(12, 225);
+            this.l_tcolour.Location = new System.Drawing.Point(6, 61);
             this.l_tcolour.Name = "l_tcolour";
             this.l_tcolour.Size = new System.Drawing.Size(50, 13);
             this.l_tcolour.TabIndex = 1;
@@ -245,14 +227,14 @@
             // 
             // tb_tcolour
             // 
-            this.tb_tcolour.Location = new System.Drawing.Point(76, 222);
+            this.tb_tcolour.Location = new System.Drawing.Point(70, 58);
             this.tb_tcolour.Name = "tb_tcolour";
-            this.tb_tcolour.Size = new System.Drawing.Size(110, 20);
+            this.tb_tcolour.Size = new System.Drawing.Size(93, 20);
             this.tb_tcolour.TabIndex = 2;
             // 
             // bt_tcolour
             // 
-            this.bt_tcolour.Location = new System.Drawing.Point(192, 221);
+            this.bt_tcolour.Location = new System.Drawing.Point(169, 58);
             this.bt_tcolour.Name = "bt_tcolour";
             this.bt_tcolour.Size = new System.Drawing.Size(20, 20);
             this.bt_tcolour.TabIndex = 7;
@@ -263,54 +245,84 @@
             // cb_gray
             // 
             this.cb_gray.AutoSize = true;
-            this.cb_gray.Enabled = false;
-            this.cb_gray.Location = new System.Drawing.Point(12, 248);
+            this.cb_gray.Location = new System.Drawing.Point(6, 6);
             this.cb_gray.Name = "cb_gray";
             this.cb_gray.Size = new System.Drawing.Size(73, 17);
             this.cb_gray.TabIndex = 8;
             this.cb_gray.Text = "Grayscale";
             this.cb_gray.UseVisualStyleBackColor = true;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tp_placeholdit);
+            this.tabControl1.Controls.Add(this.tp_placekitten);
+            this.tabControl1.Location = new System.Drawing.Point(12, 92);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 110);
+            this.tabControl1.TabIndex = 9;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tp_placeholdit
+            // 
+            this.tp_placeholdit.Controls.Add(this.l_text);
+            this.tp_placeholdit.Controls.Add(this.tb_tcolour);
+            this.tp_placeholdit.Controls.Add(this.tb_text);
+            this.tp_placeholdit.Controls.Add(this.l_tcolour);
+            this.tp_placeholdit.Controls.Add(this.tb_bgcolour);
+            this.tp_placeholdit.Controls.Add(this.bt_tcolour);
+            this.tp_placeholdit.Controls.Add(this.bt_bgcolour);
+            this.tp_placeholdit.Controls.Add(this.l_bgcolour);
+            this.tp_placeholdit.Location = new System.Drawing.Point(4, 22);
+            this.tp_placeholdit.Name = "tp_placeholdit";
+            this.tp_placeholdit.Padding = new System.Windows.Forms.Padding(3);
+            this.tp_placeholdit.Size = new System.Drawing.Size(192, 84);
+            this.tp_placeholdit.TabIndex = 0;
+            this.tp_placeholdit.Text = "placehold.it";
+            this.tp_placeholdit.UseVisualStyleBackColor = true;
+            // 
+            // tp_placekitten
+            // 
+            this.tp_placekitten.Controls.Add(this.cb_gray);
+            this.tp_placekitten.Location = new System.Drawing.Point(4, 22);
+            this.tp_placekitten.Name = "tp_placekitten";
+            this.tp_placekitten.Padding = new System.Windows.Forms.Padding(3);
+            this.tp_placekitten.Size = new System.Drawing.Size(192, 84);
+            this.tp_placekitten.TabIndex = 1;
+            this.tp_placekitten.Text = "placekitten";
+            this.tp_placekitten.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
+            this.AcceptButton = this.bt_go;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 311);
-            this.Controls.Add(this.cb_gray);
-            this.Controls.Add(this.bt_tcolour);
-            this.Controls.Add(this.bt_bgcolour);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.bt_save);
-            this.Controls.Add(this.tb_height);
-            this.Controls.Add(this.tb_width);
             this.Controls.Add(this.bt_go);
             this.Controls.Add(this.pb_image);
-            this.Controls.Add(this.tb_tcolour);
-            this.Controls.Add(this.l_tcolour);
-            this.Controls.Add(this.tb_bgcolour);
-            this.Controls.Add(this.l_bgcolour);
-            this.Controls.Add(this.tb_text);
-            this.Controls.Add(this.l_text);
-            this.Controls.Add(this.l_height);
-            this.Controls.Add(this.l_width);
-            this.Controls.Add(this.gb_service);
+            this.Controls.Add(this.gb_size);
             this.MinimumSize = new System.Drawing.Size(500, 350);
             this.Name = "Form1";
             this.Text = "Placeholder Image";
-            this.gb_service.ResumeLayout(false);
-            this.gb_service.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).EndInit();
+            this.gb_size.ResumeLayout(false);
+            this.gb_size.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tb_height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tp_placeholdit.ResumeLayout(false);
+            this.tp_placeholdit.PerformLayout();
+            this.tp_placekitten.ResumeLayout(false);
+            this.tp_placekitten.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox gb_service;
-        private System.Windows.Forms.RadioButton rb_placekitten;
-        private System.Windows.Forms.RadioButton rb_placeholdit;
+        private System.Windows.Forms.GroupBox gb_size;
         private System.Windows.Forms.Label l_width;
         private System.Windows.Forms.Label l_height;
         private System.Windows.Forms.Label l_text;
@@ -329,6 +341,9 @@
         private System.Windows.Forms.TextBox tb_tcolour;
         private System.Windows.Forms.Button bt_tcolour;
         private System.Windows.Forms.CheckBox cb_gray;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tp_placeholdit;
+        private System.Windows.Forms.TabPage tp_placekitten;
     }
 }
 
--- Placeholder Image/Form1.cs
+++ Placeholder Image/Form1.cs
@@ -23,71 +23,9 @@ namespace Placeholder_Image
         string tcolour;
         string temp;
 
-        private void rb_placeholdit_CheckedChanged(object sender, EventArgs e)
-        {
-            l_text.Enabled = true;
-            tb_text.Enabled = true;
-            l_bgcolour.Enabled = true;
-            tb_bgcolour.Enabled = true;
-            bt_bgcolour.Enabled = true;
-            l_tcolour.Enabled = true;
-            tb_tcolour.Enabled = true;
-            bt_tcolour.Enabled = true;
-            cb_gray.Enabled = false;
-        }
-
-        private void rb_placekitten_CheckedChanged(object sender, EventArgs e)
-        {
-            l_text.Enabled = false;
-            tb_text.Enabled = false;
-            l_bgcolour.Enabled = false;
-            tb_bgcolour.Enabled = false;
-            bt_bgcolour.Enabled = false;
-            l_tcolour.Enabled = false;
-            tb_tcolour.Enabled = false;
-            bt_tcolour.Enabled = false;
-            cb_gray.Enabled = true;
-        }
-
         private void bt_go_Click(object sender, EventArgs e)
         {
-            this.Width = this.MinimumSize.Width + (int)tb_width.Value - 252;
-            this.Height = this.MinimumSize.Height + (int)tb_height.Value - 285;
-
-            if (rb_placeholdit.Checked == true)
-            {
-                pb_image.Load("http://placehold.it/" + tb_width.Value + "x" + tb_height.Value + "/" + bgcolour + "/" + tcolour + "?text=" + tb_text.Text);
-                bt_save.Enabled = true;
-            }
-            else if (rb_placekitten.Checked == true)
-            {
-                if (cb_gray.Checked == true)
-                {
-                    try
-                    {
-                        pb_image.Load("http://placekitten.com/g/" + tb_width.Value + "/" + tb_height.Value);
-                        bt_save.Enabled = true;
-                    }
-                    catch
-                    {
-                        MessageBox.Show("404\nImage not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        bt_save.Enabled = false;
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        pb_image.Load("http://placekitten.com/" + tb_width.Value + "/" + tb_height.Value);
-                        bt_save.Enabled = true;
-                    }
-                    catch
-                    {
-                        MessageBox.Show("404\nImage not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        bt_save.Enabled = false;
-                    }
-                }
-            }
+            magic();
         }
 
         private void bt_save_Click(object sender, EventArgs e)
@@ -117,6 +55,59 @@ namespace Placeholder_Image
             //Löscht das # von der Hex Farbe
             tcolour = temp.Substring(1, temp.Length - 1);
             tb_tcolour.Text = tcolour;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab == tp_placeholdit)
+            {
+                tabControl1.Size = new Size (200, 110);
+            }
+            else if (tabControl1.SelectedTab == tp_placekitten)
+            {
+                tabControl1.Size = new Size(200, 50);
+            }
+        }
+
+        private void magic()
+        {
+                        this.Width = this.MinimumSize.Width + (int)tb_width.Value - 252;
+            this.Height = this.MinimumSize.Height + (int)tb_height.Value - 285;
+
+            if (tabControl1.SelectedTab == tp_placeholdit)
+            {
+                pb_image.Load("http://placehold.it/" + tb_width.Value + "x" + tb_height.Value + "/" + bgcolour + "/" + tcolour + "?text=" + tb_text.Text);
+                bt_save.Enabled = true;
+            }
+            else if (tabControl1.SelectedTab == tp_placekitten)
+            {
+                if (cb_gray.Checked == true)
+                {
+                    try
+                    {
+                        pb_image.Load("http://placekitten.com/g/" + tb_width.Value + "/" + tb_height.Value);
+                        bt_save.Enabled = true;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("404\nImage not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        bt_save.Enabled = false;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        pb_image.Load("http://placekitten.com/" + tb_width.Value + "/" + tb_height.Value);
+                        bt_save.Enabled = true;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("404\nImage not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        bt_save.Enabled = false;
+                    }
+                }
+            }
         }
     }
 }
